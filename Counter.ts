4cbf1efbb3aace79426b724fd082ebc581fa083e
BlocksOfGold <117589512+BlocksOfGold@users.noreply.github.com>
2024-05-06 19:05:15 -0500

    Update pxt.json, Counter.ts

--- Counter.ts
+++ Counter.ts
@@ -5,45 +5,45 @@ namespace counterext {
 
     //%block="Count"
     export function count(): void {
-        counter++
-    }
+        counter++;
+    };
 
     //%block="Log Counter"
     export function logcount(): void {
-        console.log(counter)
-    }
+        console.log(counter);
+    };
 
     //%block="Counter"
     export function counterobj(): number {
-        return counter
-    }
+        return counter;
+    };
 
     //%block="Count By: $value"
     export function countby(value: number): void {
         counter += value
         if (counter < 0) {
-            counter = 0
-        }
-    }
+            counter = 0;
+        };
+    };
 
     //%block="Reset Counter"
     export function resetcounter(): void {
-        counter = 0
-    }
+        counter = 0;
+    };
 
     //%block="Set Counter To: $value"
     export function setcounter(value: number): void {
         counter = value
         if (counter < 0) {
-            counter = 0
-        }
-    }
+            counter = 0;
+        };
+    };
 
     //%block="Decrease Counter"
     export function decrease(): void {
         counter -= 1
         if (counter < 0) {
-            counter = 0
-        }
-    }
-}
+            counter = 0;
+        };
+    };
+};
